7e225bee5aab3298680eb1348367cd6dcdefc878
itsMontoya <josh@montoya.io>
2018-05-30 20:39:00 -0700

    Implement Errorlist.Push method

--- errorlist.go
+++ errorlist.go
@@ -1,27 +1,46 @@
 package errors
 
 // ErrorList represents a list of errors
-type ErrorList []error
+type ErrorList struct {
+	errs []error
+}
 
 // Err will return an error value if the ErrorList isn't empty
-func (e ErrorList) Err() (err error) {
-	if len(e) == 0 {
+func (e *ErrorList) Err() (err error) {
+	if len(e.errs) == 0 {
 		return
 	}
 
 	return e
 }
 
-func (e ErrorList) Error() (out string) {
-	switch len(e) {
+// Push will push an error to the error list
+func (e *ErrorList) Push(err error) {
+	if err == nil {
+		return
+	}
+
+	e.errs = append(e.errs, err)
+}
+
+// Copy will copy a slice of errors to the error list
+func (e *ErrorList) Copy(errs []error) {
+	for _, err := range errs {
+		e.Push(err)
+	}
+}
+
+// Error will return the error string value
+func (e *ErrorList) Error() (out string) {
+	switch len(e.errs) {
 	case 0:
 		return
 	case 1:
-		return e[0].Error()
+		return e.errs[0].Error()
 	}
 
 	var bs []byte
-	for _, err := range e {
+	for _, err := range e.errs {
 		bs = append(bs, []byte(err.Error())...)
 		bs = append(bs, ',', '\n')
 	}
